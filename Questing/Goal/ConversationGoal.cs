46be7ab34d8c43acc9e573c4ad15a1602d95339d
ValentinGurkov <valentingurkov@gmail.com>
2019-11-23 21:05:13 +0200

    identation

--- Questing/Goal/ConversationGoal.cs
+++ Questing/Goal/ConversationGoal.cs
@@ -3,25 +3,25 @@ using RPG.NPC;
 using UnityEngine;
 
 namespace RPG.Questing {
-  public class ConversationGoal : Goal {
-    public string Target;
+    public class ConversationGoal : Goal {
+        public string Target;
 
-    public ConversationGoal(Stage stage, string target, string description) {
-      Stage = stage;
-      Target = target;
-      Description = description;
-    }
+        public ConversationGoal(Stage stage, string target, string description) {
+            Stage = stage;
+            Target = target;
+            Description = description;
+        }
 
-    public override void Init() {
-      base.Init();
-      EventSystem.OnTalkedToNPC += TalkedToNPC;
-    }
+        public override void Init() {
+            base.Init();
+            EventSystem.OnTalkedToNPC += TalkedToNPC;
+        }
 
-    private void TalkedToNPC(DialogueInitiator npc) {
-      if (Stage.Active && npc.name.Contains(Target)) {
-        Complete();
-        EventSystem.OnTalkedToNPC -= TalkedToNPC;
-      }
+        private void TalkedToNPC(DialogueInitiator npc) {
+            if (Stage.Active && npc.name.Contains(Target)) {
+                Complete();
+                EventSystem.OnTalkedToNPC -= TalkedToNPC;
+            }
+        }
     }
-  }
 }
